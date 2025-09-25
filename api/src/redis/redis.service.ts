3ac5a84320ac6cd5955682f738a8f1129e133943
atluixx <luizz.developer@gmail.com>
2025-09-25 22:38:40 +0200

    patch: changed from redis to node-cache

--- api/src/redis/redis.service.ts
+++ api/src/redis/redis.service.ts
@@ -1,38 +1,20 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
-import { createClient, RedisClientType } from "redis";
+import NodeCache from "node-cache";
 
 @Injectable()
-export class RedisService implements OnModuleInit, OnModuleDestroy {
-  private client: RedisClientType;
-
-  async onModuleInit() {
-    this.client = createClient({
-      url: "redis://localhost:6379",
-    });
-
-    this.client.on("error", (err) => {
-      console.error("❌ Redis Client Error:", err);
-    });
-
-    await this.client.connect();
-  }
-
-  async onModuleDestroy() {
-    if (this.client) {
-      await this.client.quit();
-    }
-  }
+export class RedisService {
+  private client = new NodeCache({ checkperiod: 200 });
 
   async set(key: string, value: string, ttlSeconds?: number) {
     if (ttlSeconds) {
-      await this.client.set(key, value, { EX: ttlSeconds });
+      this.client.set(key, value, ttlSeconds);
     } else {
-      await this.client.set(key, value);
+      this.client.set(key, value);
     }
   }
 
-  async get(key: string): Promise<string | null> {
-    return this.client.get(key);
+  async get(key: string): Promise<string | null | undefined> {
+    return await this.client.get(key);
   }
 
   async del(key: string): Promise<number> {
